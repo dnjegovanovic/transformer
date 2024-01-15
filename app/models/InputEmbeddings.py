a48a066a9df8bcdfbb8fbaa4d4f49fdf795babbc
dusannj <dusan.njegovnaovic95@gmail.com>
2024-01-15 17:22:56 +0100

    Input embeddings

---
+++ app/models/InputEmbeddings.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+import math
+
+class InputEmbeddings(nn.Module):
+    
+    def __init__(self, emb_size: int, vocab_size: int) -> None:
+        """_summary_
+
+        Args:
+            emb_size (int): size of embedded vector mostly 512
+            vocab_size (int): vocabulary size
+        """
+        super().__init__()
+        
+        self.emb_size = emb_size
+        self.vocab_size = vocab_size
+        
+        self.embedding = nn.Embedding(vocab_size, emb_size)
+    def forward(self, x):
+        return self.embedding * math.sqrt(self.emb_size) # from the paper multiplay embedded by sqrt of the emb_size(d_model)
+    
+    
+class PositionalEncoding(nn.Module):
+    def __init__(self, emb_size: int, seq_len: int, dropout: float) -> None:
+        super().__init__()
+        
+        self.emb_size = emb_size
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+        
+        # creting matrix shape of
+        
+    
