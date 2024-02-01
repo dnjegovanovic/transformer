ffdb0a83d9148219357e9a9053d4bad86921864b
dusannj <dusan.njegovnaovic95@gmail.com>
2024-02-01 15:07:02 +0100

    IMplemented proijection layer.

--- app/models/Decoder.py
+++ app/models/Decoder.py
@@ -17,7 +17,7 @@ class DecoderBlock(nn.Module):
         self.cross_attn_block = cross_attn
         self.ffn = ff_net
         self.res_connection = nn.ModuleList(ResCon(dropout) for _ in range(3))
-    
+
     def forward(self, x, encoder_output, encoder_mask, decoder_mask):
         """_summary_
 
@@ -27,13 +27,21 @@ class DecoderBlock(nn.Module):
             ecoder_mask (_type_): mask for language 1
             decoder_mask (_type_): mask for language 2
         """
-        
-        x = self.res_connection[0](x, lambda x: self.self_attn_block(x,x,x, decoder_mask))
-        x = self.res_connection[1](x, lambda x: self.cross_attn_block(x,encoder_output,encoder_output, encoder_mask))
+
+        x = self.res_connection[0](
+            x, lambda x: self.self_attn_block(x, x, x, decoder_mask)
+        )
+        x = self.res_connection[1](
+            x,
+            lambda x: self.cross_attn_block(
+                x, encoder_output, encoder_output, encoder_mask
+            ),
+        )
         x = self.res_connection[1](x, lambda x: self.ffn(x))
-        
+
         return x
-    
+
+
 class Decoder(nn.Module):
     def __init__(self, layers: nn.ModuleList) -> None:
         super().__init__()
