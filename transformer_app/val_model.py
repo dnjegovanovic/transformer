ed4f2fa7024b459219935be6f72c9ea37763403a
dusannj <dusan.njegovnaovic95@gmail.com>
2024-03-12 23:48:51 +0100

    Visualize attention

--- transformer_app/val_model.py
+++ transformer_app/val_model.py
@@ -6,7 +6,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 if __name__ == "__main__":
     model_ckpt_file = Path(
-        r"test_dir\test_03\version_1\checkpoints\epoch=19-step=36380.ckpt"
+        r"test_dir\test_01\version_1\checkpoints\model-epoch=09-val_loss=4.56244.ckpt"
     )
 
     model = TransformerModule.load_from_checkpoint(model_ckpt_file, batch_size=1)
